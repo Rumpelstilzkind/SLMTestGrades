8689f238d7197e86411d6dee19e7ecfac532c1f6
Rumpelstilzkind <ne.mes@gmx.at>
2022-12-21 18:09:36 +0100

    Initial commit

---
+++ src/main/java/at/fhtw/bic/slmtestgrades/SlmTestGradesApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.bic.slmtestgrades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmTestGradesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmTestGradesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/fhtw/bic/slmtestgrades/SlmTestGradesApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.bic.slmtestgrades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmTestGradesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
